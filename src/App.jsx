edcf558b46374da1615759e9aad3af442295a611
Federico Amormino <federico.amormino@gmail.com>
2022-04-23 19:46:43 -0300

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import Die from "./components/Die";
+
 export default function App() {
-  return <main />;
+  return (
+    <main>
+      <div className="dice-container">
+        <Die number={1} />
+        <Die number={2} />
+        <Die number={3} />
+        <Die number={4} />
+        <Die number={5} />
+        <Die number={6} />
+        <Die number={1} />
+        <Die number={2} />
+        <Die number={3} />
+        <Die number={4} />
+      </div>
+    </main>
+  );
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props) {
+  return (
+    <div className="die-face">
+      <h2 className="die-num">{props.number}</h2>
+    </div>
+  );
+}
