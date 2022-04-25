6985913cb5dd11e5265fdadc955db425b07fed68
Federico Amormino <federico.amormino@gmail.com>
2022-04-25 11:49:21 -0300

    Adding roll button

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,19 @@ export default function App() {
 
     return newDice;
   }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
   console.log(allNewDice());
   const diceElements = dice.map((die) => <Die number={die} />);
 
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
