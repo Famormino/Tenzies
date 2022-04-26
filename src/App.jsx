b6cfcb4a06df39d22dbf5248c29608a7bfbbaf17
Federico Amormino <federico.amormino@gmail.com>
2022-04-26 12:40:07 -0300

    Selecting Id from parent

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,23 @@ import Die from "./components/Die";
 export default function App() {
   const [dice, setDice] = useState(allNewDice());
 
+  /**
+   * Challenge: Create a function `holdDice` that takes
+   * `id` as a parameter. For now, just have the function
+   * console.log(id).
+   *
+   * Then, figure out how to pass that function down to each
+   * instance of the Die component so when each one is clicked,
+   * it logs its own unique ID property. (Hint: there's more
+   * than one way to make that work, so just choose whichever
+   * you want)
+   *
+   */
+
+  function holdDice(id) {
+    console.log(id);
+  }
+
   function allNewDice() {
     const newDice = [];
 
@@ -23,7 +40,15 @@ export default function App() {
   function rollDice() {
     setDice(allNewDice());
   }
-  const diceElements = dice.map((die) => <Die key={die.id} number={die.value} />);
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      holdDice={() => holdDice(die.id)}
+      id={die.id}
+      isHeld={die.isHeld}
+      number={die.value}
+    />
+  ));
 
   return (
     <main>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,14 @@
 export default function Die(props) {
+  console.log(props);
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : null,
+  };
+
   return (
-    <div className="die-face">
-      <h2 className="die-num">{props.number}</h2>
+    <div className="die-face" style={styles}>
+      <h2 className="die-num" onClick={props.holdDice}>
+        {props.number}
+      </h2>
     </div>
   );
 }
